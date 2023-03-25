cd8900551e36f9521fcdd6ddba5bf521fb32b3a1
dmitryfd <dmitrii.cucleschin@gmail.com>
2023-03-24 20:09:01 -0700

    remove router from the repro

--- app/another-page/page.tsx
+++ app/another-page/page.tsx
@@ -1,15 +1,13 @@
 import ClientComponent from "@/ClientComponent";
-import { cookies } from "next/headers";
 
 export const metadata = {
   title: 'Another page'
 }
 
 export default function AnotherPage() {
-  const cookieValue = cookies().get('some-cookie')?.value ?? 'unknown';
   return (
     <ClientComponent>
-      <p>{cookieValue}</p>
+      <p>Hello from another page</p>
     </ClientComponent>
   )
 }
